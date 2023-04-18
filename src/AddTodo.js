114891c028abae6c7b002d20ddb20b42c1321620
Jo Ah Hyeon <ah2974@sookmyung.ac.kr>
2023-04-18 17:45:26 +0900

    10-2 AddTodo.js

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -4,8 +4,7 @@ class AddTodo extends React.Component {
     constructor(props){ 
         super(props); 
         this.state ={item :{title:""}}; 
-        this.add = props.add; 
-       
+        this.add = props.add;// props 의 함수를 this.add 에 연결, props에는 상위 컴포넌트(App.js)의 함수, 매개변수가 들어 있음. 
     } 
     onInputChange =(e)=>{ 
         const thisItem =this.state.item; 
@@ -13,6 +12,7 @@ class AddTodo extends React.Component {
         this.setState({item:thisItem}); 
         console.log(thisItem); 
     }
+
     onButtonClick =()=>{ 
         this.add(this.state.item); 
         this.setState({item:{title:""}});// text 값을 추가하고 입력 필드는 초기화시킨다. 
@@ -28,27 +28,27 @@ class AddTodo extends React.Component {
              <Paper style={{margine:16, padding:16}}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{paddingRight:16}}>
-                    <TextField
-                    placehoder="Add Todo here"
-                    fullWidth
-                    onChange={this.onInputChange}
-                    value={this.state.item.title}
-                    onKeyPress={this.enterKeyEventHandler}
-                    />
+                        <TextField
+                        placehoder="Add Todo here" 
+                        fullWidth
+                        onChange={this.onInputChange}
+                        value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHandler}
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button
+                        fullWidth
+                        color="secondary"
+                        variant="outlined"
+                        onClick={this.onButtonClick}
+                        >
+                            +
+                        </Button>
+                    </Grid>
                 </Grid>
-                <Grid xs={1} md={1} item>
-                <Button
-                fullWidth
-                color="secondary"
-                variant="outlined"
-                onClick={this.onButtonClick}
-                >
-                    +
-                </Button>
-            </Grid>
-        </Grid>
-    </Paper>
- ); 
-}
+            </Paper>
+        ); 
+    }
 }
 export default AddTodo;
